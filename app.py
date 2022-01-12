285f7695ac574692d78da0bf6a43a9c9f60816e6
ToniEdwards <90237519+ToniEdwards@users.noreply.github.com>
2022-01-12 01:56:19 -0600

    Working app

--- app.py
+++ app.py
@@ -1,38 +1,21 @@
 from flask import Flask, render_template, request, redirect, jsonify, make_response
-import scrape_states
-import requests
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    #home page for info and navigation
     return render_template("index.html")
 
-@app.route("/scrape")
-def scrape():
-    state_codes = scrape_states.scrape()
-    severity = []
-    event = []
-    urgency = []
-    message = []
-    title = []
-
-    for state in state_codes:
-        weather_response = requests.get(f"https://api.weather.gov/alerts/active?status=actual&message_type=alert&area={state}&severity=Severe").json()
-        severity.append(weather_response["features"][0]["properties"]["severity"])
-        event.append(weather_response["features"][0]["properties"]["event"])
-        urgency.append(weather_response["features"][0]["properties"]["urgency"])
-        message.append(weather_response["features"][0]["properties"]["message"])
-        title.append(weather_response["features"]["title"])
-
-    return redirect("/")
-
-@app.route("/charts.html")
+@app.route("/charts")
 def charts():
+    #display drop down menu with states list
+    #accept the state selection and display charts
     return  render_template("charts.html")
 
-@app.route("/map.html")
+@app.route("/map")
 def map():
+    #display drop down menu with states list
+    #accept the state selection and display map
     return render_template("map.html")    
 
 if __name__ == '__main__':
