0fe1ea247571314c6ccdce8cdca3f39cbfe5eb25
ToniEdwards <90237519+ToniEdwards@users.noreply.github.com>
2022-01-12 01:52:18 -0600

    Uploading files for Web scrapping and API call

--- scrape_states.py
+++ scrape_states.py
@@ -1,17 +1,11 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
 
 def scrape():
     url = "https://www23.statcan.gc.ca/imdb/p3VD.pl?Function=getVD&TVD=53971"
     html = pd.read_html(url)
 
     states_table = html[0]
-
+    states = []
     states = states_table["Alpha code"]
 
     return states
