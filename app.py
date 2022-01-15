9aba9196e43dfff4b7b4cb71e26ac47fe58c891b
Toni <mstonielt@gmail.com>
2022-01-15 12:01:34 -0600

    Pushing some updated files Saturday 1-15

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
-from flask import Flask, render_template, request, redirect, jsonify, make_response
+from werkzeug.utils import secure_filename
+import weather_data
 import scrape_states
-import requests
-
+import pandas as pd
+from flask import Flask, render_template, request, redirect, jsonify, make_response
 app = Flask(__name__)
+
+state_codes = scrape_states.scrape()
+print("This is the state code list:", state_codes)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    #home page for info and navigation
+    return render_template("index.html", states = state_codes)
 
-@app.route("/scrape")
-def scrape():
-    state_codes = scrape_states.scrape()
-    severity = []
-    event = []
-    urgency = []
-    message = []
-    title = []
-
-    for state in state_codes:
-        weather_response = requests.get(f"https://api.weather.gov/alerts/active?status=actual&message_type=alert&area={state}&severity=Severe").json()
-        severity.append(weather_response["features"][0]["properties"]["severity"])
-        event.append(weather_response["features"][0]["properties"]["event"])
-        urgency.append(weather_response["features"][0]["properties"]["urgency"])
-        message.append(weather_response["features"][0]["properties"]["message"])
-        title.append(weather_response["features"]["title"])
-
-    return redirect("/")
-
-@app.route("/charts.html")
+@app.route("/charts")
 def charts():
+    #display drop down menu with states list
+    #accept the state selection and display charts
     return  render_template("charts.html")
 
-@app.route("/map.html")
+@app.route("/map")
 def map():
+    #display drop down menu with states list
+    #accept the state selection and display map
     return render_template("map.html")    
 
 if __name__ == '__main__':
