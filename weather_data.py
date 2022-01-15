9636ade6293903201ac196d6d8b36f6d3f9b837e
ToniEdwards <90237519+ToniEdwards@users.noreply.github.com>
2022-01-15 14:35:37 -0600

    Update Weather Data call query for single state

--- weather_data.py
+++ weather_data.py
@@ -1,19 +1,22 @@
 import requests
 import scrape_states
 
-def scrape():
+def api(state):
     city = []
     severity = []
     event = []
     urgency = []
     headline = []
     coordinates = []
-    states = scrape_states.scrape()
-    
+    #states = scrape_states.scrape()
     error = 0
-    for state in states:
-        try:
-            weather_response = requests.get(f"https://api.weather.gov/alerts/active?status=actual&message_type=alert&area={state}&severity=Severe").json()
+    
+    
+    try:
+            query = f"https://api.weather.gov/alerts/active?status=actual&message_type=alert&area={state}&severity=Severe"
+            weather_response = requests.get(query).json()
+            print(state)
+            print(query)
             if len(weather_response["features"]) > 0:
                 impacts = len(weather_response["features"])
                 for x in range(impacts):
@@ -23,8 +26,8 @@ def scrape():
                     urgency.append(weather_response["features"][x]["properties"]["urgency"])
                     headline.append(weather_response["features"][x]["properties"]["headline"])
                     coordinates.append(weather_response["features"][x]["geometry"]["coordinates"])
-        except KeyError:
+    except KeyError:
             error = error + 1
-        except TypeError:
+    except TypeError:
             error = error + 1
-    return weather_response       
+    return weather_response      
