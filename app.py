f75cb3d0175bf54266d1b818af6c88a95309232d
ToniEdwards <90237519+ToniEdwards@users.noreply.github.com>
2022-01-15 12:22:48 -0600

    Removing the duplicate issue

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from werkzeug.utils import secure_filename
 import weather_data
 import scrape_states
@@ -27,27 +26,4 @@ def map():
     return render_template("map.html")    
 
 if __name__ == '__main__':
-=======
-from flask import Flask, render_template, request, redirect, jsonify, make_response
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    #home page for info and navigation
-    return render_template("index.html")
-
-@app.route("/charts")
-def charts():
-    #display drop down menu with states list
-    #accept the state selection and display charts
-    return  render_template("charts.html")
-
-@app.route("/map")
-def map():
-    #display drop down menu with states list
-    #accept the state selection and display map
-    return render_template("map.html")    
-
-if __name__ == '__main__':
->>>>>>> 70cc9b32eb64e442478b9836b2c5497b838574a5
     app.run()
